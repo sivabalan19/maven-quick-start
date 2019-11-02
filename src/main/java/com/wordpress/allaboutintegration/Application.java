e7d885a2217407066642ebfd0f6f99ff36216fa7
Siva V <sivabalan19@gmail.com>
2019-11-02 20:42:59 +0100

    added my first project from basics

---
+++ src/main/java/com/wordpress/allaboutintegration/Application.java
@@ -0,0 +1,13 @@
+package com.wordpress.allaboutintegration;
+
+public class Application {
+
+    public Application(){
+        System.out.println("Application is starting...");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Starting all the applications..");
+        Application app1 = new Application();
+    }
+}
